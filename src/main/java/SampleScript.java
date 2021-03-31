a985e93cdcb37b13668fd05f7afcb4ae41556bac
Zubry <zubryjs@gmail.com>
2021-03-31 19:37:12 -0400

    Add sample script

---
+++ src/main/java/SampleScript.java
@@ -0,0 +1,15 @@
+import org.dreambot.api.script.AbstractScript;
+import org.dreambot.api.script.Category;
+import org.dreambot.api.script.ScriptManifest;
+
+@ScriptManifest(name = "Script Name", description = "My script description!", author = "Developer Name",
+                version = 1.0, category = Category.WOODCUTTING, image = "")
+public class SampleScript extends AbstractScript {
+    @Override
+    public int onLoop() {
+        MethodProvider.log("My first script!");
+        return 1000;
+    }
+}
+
+
