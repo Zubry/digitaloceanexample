734c4c96da811c0d36b73093ab8e07ebb04fd08c
Zubry <zubryjs@gmail.com>
2021-03-31 19:41:12 -0400

    Import method provider

--- src/main/java/SampleScript.java
+++ src/main/java/SampleScript.java
@@ -1,7 +1,7 @@
 import org.dreambot.api.script.AbstractScript;
 import org.dreambot.api.script.Category;
 import org.dreambot.api.script.ScriptManifest;
-import org.dreambot.api.script.MethodProvider;
+import org.dreambot.api.methods.MethodProvider;
 
 @ScriptManifest(name = "Script Name", description = "My script description!", author = "Developer Name",
                 version = 1.0, category = Category.WOODCUTTING, image = "")
