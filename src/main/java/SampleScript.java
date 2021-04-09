7bb6b776002561b54596c199e78501d43b71e242
jamessteele <jamessteele@IT-ES-002AM.local>
2021-04-09 16:04:52 -0400

    Fix code

--- src/main/java/SampleScript.java
+++ src/main/java/SampleScript.java
@@ -1,11 +1,28 @@
+import java.awt.Graphics2D;
+
 import org.osbot.rs07.script.Script;
 import org.osbot.rs07.script.ScriptManifest;
 
 @ScriptManifest(name="SampleScript", author="Sample", version=1.0, info="", logo="")
-
 public class SampleScript extends Script {
 	@Override
 	public void onStart() {
 		log("Script started");
+	}
+
+	@Override
+	public int onLoop() throws InterruptedException {
+		log("Looping...");
+		return 1000;
+	}
+
+	@Override
+	public void onExit() {
+		log("Exiting...")
+	}
+
+	@Override
+	public void onPaint(Graphics2D g) {
+
 	}
 }
